Fixed observation probability calculation

hmm.py
@@ -106,11 +106,6 @@ class HMM:
         posteriors = forward * backward / np.sum(forward * backward, axis=1, keepdims=True)
         return posteriors
     
-    def observation_probability(self, obs_seq):
-        alpha = self.forward(obs_seq)
-
-        return np.sum(alpha[-1,:])
-
     def observation_prob(self, obs_seq):
         """ P( entire observation sequence | A, B, pi ) """
         return np.sum(self._forward(obs_seq)[:, -1])
@@ -219,6 +214,7 @@ def example():
     # result = umbrellaHMM.forward(umbrella_evidence)
     # result = umbrellaHMM.backward(umbrella_evidence)
     result = umbrellaHMM.forward_backward(umbrella_evidence)
+    # result = umbrellaHMM.viterbi(umbrella_evidence)
     return (result)
 
 

main.py
@@ -283,10 +283,10 @@ def run_russel_norvig_world_single_policy_only_with_random_sample_observations(T
     russelhmm = hmm.HMM(np.array(trans_p), np.array(emit_p), np.array(start_p))
     
     B = []
-    for i in range(1):
+    for i in range(2):
         obs = hlp.execute_policy(p, T, start_state, 12)
         obs = [obs[i] + 1 for i in range(len(obs))]
-        prob = russelhmm.observation_probability(obs)
+        prob = russelhmm.observation_prob(obs)
         B.append((obs, prob))
     print(B)
     