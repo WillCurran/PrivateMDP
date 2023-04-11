Updated tests for forward backward and kl divergence calculation. Using hmm observation probability calculation. Upper bound still increasing with larger number of observations processed. Bug detected where negative remaining probability is computed.

helpers.py
@@ -83,7 +83,7 @@ def take_action(curr_state, action, T):
     coin = random.random()
     # coin = 0.5
     # 12 possible next states
-    next_states = T[curr_state, :, int(action)]
+    next_states = T[curr_state,:, int(action)]
     prob_counter = 0.0
     # randomly take next action based on weights
     for state, prob in enumerate(next_states):
@@ -273,7 +273,7 @@ def equilibrium_distribution(transition_matrix, max_iter=100, tol=1e-8):
         prev_distribution = distribution
         # Calculate new distribution
         distribution = np.sum(
-            transition_matrix[:, :, i] @ distribution, axis=1)
+            transition_matrix[:,:, i] @ distribution, axis=1)
         # Check for convergence
         if np.linalg.norm(distribution - prev_distribution) < tol:
             break
@@ -369,7 +369,7 @@ def enumerate_policies(states, actions, obstacles, terminals):
     # Enumerate the policies of the MDP
     policies = enumerate_policies(mdp)
     """
-    number_of_policies = len(actions) ** (len(states) -
+    number_of_policies = len(actions) ** (len(states) - 
                                           len(obstacles) - len(terminals))
     # Create the 2-d array of possible selections
     selections = [actions for i in range(len(states))]
@@ -448,8 +448,22 @@ def print_table(table):
     print(df.to_string())
 
 
+def kl_divergence_example():
+    p = [9 / 25, 12 / 25, 4 / 25, 0, 0]
+    q = [1 / 3, 1 / 3, 1 / 3, 0, 0]
+
+    result_1 = np.sum(kl_div(p, q), axis=-1)
+    result_2 = np.sum(kl_div(q, p), axis=-1)
+    
+    result_3 = kl_divergence_for_each_state(p, q)
+    result_4 = kl_divergence_for_each_state(q, p)
+    
+    return (result_1, result_2, np.sum(result_3[2]), np.sum(result_4[2]))
+
+
 def main():
     print("Calling main function in helpers.py")
+    print(kl_divergence_example())
 
 
 if __name__ == "__main__":

hmm.py
@@ -1,6 +1,5 @@
 # https://github.com/aehuynh/hidden-markov-model
 
-
 import numpy as np
 import pandas as pd
 
@@ -57,7 +56,7 @@ class HMM:
 
         for t in reversed(range(T - 1)):
             for n in range(N):
-                X[n, t] = np.sum(X[:, t + 1] * self.A[n, :]
+                X[n, t] = np.sum(X[:, t + 1] * self.A[n,:]
                                  * self.B[:, obs_seq[t + 1]])
 
         return X
@@ -74,12 +73,12 @@ class HMM:
 
         F = np.zeros((T, N))
 
-        F[0, :] = self.pi * self.B[:, obs_seq[0]]
+        F[0,:] = self.pi * self.B[:, obs_seq[0]]
         F[0] = F[0] / F[0].sum()
 
         for t in range(1, T):
             for n in range(N):
-                F[t, n] = np.dot(F[t - 1, :], (self.A[:, n])
+                F[t, n] = np.dot(F[t - 1,:], (self.A[:, n])
                                  ) * self.B[n, obs_seq[t]]
             # Normalize
             sum_F = F[t].sum()
@@ -103,11 +102,11 @@ class HMM:
         T = len(obs_seq)
 
         X = np.zeros((T, N))
-        X[-1:, :] = 1
+        X[-1:,:] = 1
 
         for t in reversed(range(T - 1)):
             for n in range(N):
-                X[t, n] = np.sum(X[t + 1, :] * self.A[n, :]
+                X[t, n] = np.sum(X[t + 1,:] * self.A[n,:]
                                  * self.B[:, obs_seq[t + 1]])
             # Normalize
             sum_X = X[t].sum()
@@ -118,7 +117,7 @@ class HMM:
         first = np.zeros((1, N))
 
         for n in range(N):
-            first[0][n] = np.sum(X[0, :] * self.A[n, :]
+            first[0][n] = np.sum(X[0,:] * self.A[n,:]
                                  * self.B[:, obs_seq[0]])
         first = first / first.sum()
         return np.concatenate([first, X])
@@ -217,13 +216,13 @@ class HMM:
 
         xi = np.zeros((T - 1, N, N))
         for t in range(xi.shape[0]):
-            xi[t, :, :] = self.A * forw[:, [t]] * \
+            xi[t,:,:] = self.A * forw[:, [t]] * \
                 self.B[:, obs_seq[t + 1]] * back[:, t + 1] / obs_prob
 
         gamma = forw * back / obs_prob
 
         # Gamma sum excluding last column
-        gamma_sum_A = np.sum(gamma[:, :-1], axis=1, keepdims=True)
+        gamma_sum_A = np.sum(gamma[:,:-1], axis=1, keepdims=True)
         # Vector of binary values indicating whether a row in gamma_sum is 0.
         # If a gamma_sum row is 0, save old rows on update
         rows_to_keep_A = (gamma_sum_A == 0)
@@ -291,22 +290,24 @@ class HMM:
 
 
 def example():
-    umbrella_transition = [[0.7, 0.3], [0.3, 0.7]]
-    umbrella_sensor = [[0.9, 0.1], [0.2, 0.8]]
-    umbrella_initial = [0.5, 0.5]
+    umbrella_transition = [[0.7, 0.3, 0], [0.3, 0.7, 0], [0, 0, 0]]
+    umbrella_sensor = [[0.9, 0.1, 0], [0.2, 0.8, 0], [0, 0, 0]]
+    umbrella_initial = [0.5, 0.5, 0]
     umbrellaHMM = HMM(np.array(umbrella_transition), np.array(
         umbrella_sensor), np.array(umbrella_initial))
     # {umbrella, umbrella, no umbrella, umbrella, umbrella}
     umbrella_evidence = [0, 0, 1, 0, 0]
-    # result = umbrellaHMM.forward(umbrella_evidence)
-    # result = umbrellaHMM.backward(umbrella_evidence)
-    # result = umbrellaHMM.forward_backward(umbrella_evidence)
+    result_1 = umbrellaHMM.forward(umbrella_evidence)
+    result_2 = umbrellaHMM.backward(umbrella_evidence)
+    result_3 = umbrellaHMM._forward(umbrella_evidence)
+    result_4 = umbrellaHMM._backward(umbrella_evidence)
+    result_5 = umbrellaHMM.forward_backward(umbrella_evidence)
     V, prev = umbrellaHMM.viterbi(umbrella_evidence)
     last_state = np.argmax(V[:, -1])
     path = list(umbrellaHMM.build_viterbi_path(prev, last_state))[::-1]
     print(path)
-    result = umbrellaHMM.viterbi_beam(umbrella_evidence, 10)
-    return result
+    result_6 = umbrellaHMM.viterbi_beam(umbrella_evidence, 10)
+    return ('1', result_1, '2', result_2, '3', result_3, '4', result_4, '5', result_5, '6', result_6)
 
 
 def main():

main.py
@@ -118,8 +118,8 @@ def examine_russel_norvig_world_seperated(exponent=3):
             policy_list = list(
                 map(lambda x: np.nan if x == 'nan' else int(x), policy_str[1:-1].split(', ')))
             # Call the function with n and get the result
-            # utility, upper_bound, lower_bound = run_russel_norvig_world_single_policy_only_with_random_sample_observations(T, policy_list, r, gamma, int(n))
-            utility, upper_bound, lower_bound = run_russel_norvig_world_single_policy_only(T, policy_list, r, gamma, int(n))
+            utility, upper_bound, lower_bound = run_russel_norvig_world_single_policy_only_with_random_sample_observations(T, policy_list, r, gamma, int(n))
+            # utility, upper_bound, lower_bound = run_russel_norvig_world_single_policy_only(T, policy_list, r, gamma, int(n))
 
             # Append the result and other object properties to the result list
 
@@ -131,6 +131,7 @@ def examine_russel_norvig_world_seperated(exponent=3):
                 'Upper Bound': upper_bound,
                 'exponent': j
             }
+
             result_array[i][j] = result
             # Create an empty list to hold the 'exponent' values
     # Iterate over the columns in the 2D array of objects
@@ -352,10 +353,10 @@ def run_russel_norvig_world_single_policy_only(T, p, r, gamma, k):
     markov_chain_df = pd.DataFrame(markov_chain)
     # print(markov_chain_df.to_string())
     # set obstacles to loop
-    markov_chain[5][5] = 1.0
+    # markov_chain[5][5] = 1.0
     # set terminal state to loop
-    markov_chain[3][3] = 1.0
-    markov_chain[7][7] = 1.0
+    # markov_chain[3][3] = 1.0
+    # markov_chain[7][7] = 1.0
 
     # hlp.print_h2("create hidden markov model with mdp and policy")
     start_state = 8
@@ -387,9 +388,9 @@ def run_russel_norvig_world_single_policy_only(T, p, r, gamma, k):
     # print(least_likely_future_state)
     # hlp.print_h2('posterior marginals')
     # Set obstacle states to loop
-    trans_p[5][5] = 1.0
+    # trans_p[5][5] = 1.0
     # Set Terminal states to loop
-    trans_p[7][7] = 1.0
+    # trans_p[7][7] = 1.0
     # trans_p[end_state][end_state] = 1.0
     russelhmm = hmm.HMM(np.array(trans_p), np.array(emit_p), np.array(start_p))
 
@@ -419,8 +420,8 @@ def run_russel_norvig_world_single_policy_only(T, p, r, gamma, k):
     divergences = np.zeros(length)
     for i, a in enumerate(A):
         obs = a[2]
-        probabilities[i] = a[0]
         obs = [obs[i] + 1 for i in range(len(obs))]
+        probabilities[i] = russelhmm.observation_prob(obs)  # a[0]
         russelhmm = hmm.HMM(np.array(trans_p), np.array(
             emit_p), np.array(start_p))
         posterior_marginals = russelhmm.forward_backward(obs)
@@ -437,7 +438,7 @@ def run_russel_norvig_world_single_policy_only(T, p, r, gamma, k):
     # print(len(divergences))
     # print(divergences)
     expected_leakage = divergences * probabilities
-
+    print(expected_leakage)
     most_surprising_dist = np.zeros(len(states))
     most_surprising_dist[least_likely_future_state] = 1.0
 
@@ -445,10 +446,16 @@ def run_russel_norvig_world_single_policy_only(T, p, r, gamma, k):
     # print(most_surprising_dist)
     p = np.array([most_surprising_dist])
     q = np.array([future_dist])
+    print(least_likely_future_state)
+    print(p)
+    print(q)
     _p, _q, most_surprising_divergence = hlp.kl_divergence_for_each_state(p, q)
     remaining_probability = 1 - sum(probabilities)
+    print(remaining_probability)
     expected_kl_divergence = sum(most_surprising_divergence[-1])
+    print(expected_kl_divergence)
     remaining_possible_leakage = expected_kl_divergence * remaining_probability
+    print(remaining_possible_leakage)
     # print('remaining possible leakage')
     # print(remaining_possible_leakage)
     # print('Upper Bound:')
@@ -479,10 +486,10 @@ def run_russel_norvig_world_single_policy_only_with_random_sample_observations(T
     markov_chain_df = pd.DataFrame(markov_chain)
     # print(markov_chain_df.to_string())
     # set obstacles to loop
-    markov_chain[5][5] = 1.0
+    # markov_chain[5][5] = 1.0
     # set terminal state to loop
-    markov_chain[3][3] = 1.0
-    markov_chain[7][7] = 1.0
+    # markov_chain[3][3] = 1.0
+    # markov_chain[7][7] = 1.0
 
     # hlp.print_h2("create hidden markov model with mdp and policy")
     start_state = 8
@@ -514,9 +521,9 @@ def run_russel_norvig_world_single_policy_only_with_random_sample_observations(T
     # print(least_likely_future_state)
     # hlp.print_h2('posterior marginals')
     # Set obstacle states to loop
-    trans_p[5][5] = 1.0
+    # trans_p[5][5] = 1.0
     # Set Terminal states to loop
-    trans_p[7][7] = 1.0
+    # trans_p[7][7] = 1.0
     # trans_p[end_state][end_state]
 
     russelhmm = hmm.HMM(np.array(trans_p), np.array(emit_p), np.array(start_p))